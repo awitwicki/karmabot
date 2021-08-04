2b80e16138db8e34b9b3691cf2b1b55bd5e58142
Andrew Witwicki <vitvitskiy3@gmail.com>
2021-08-04 16:57:00 +0200

    Add antiflood, small clean code

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from mats_counter import count_mats
 conf = Config('congfig.ini', ['telegram_token','destruction_timeout','database_filename'])
 
 bot_token = conf.Data['telegram_token']
+flood_timeout = int(conf.Data['flood_timeout'])
 destruction_timeout = int(conf.Data['destruction_timeout'])
 database_filename = conf.Data['database_filename']
 
@@ -33,6 +34,19 @@ dp: Dispatcher = Dispatcher(bot)
 
 bot_id = bot.id
 
+users_messages = {}
+
+def is_flood_message(message: types.Message):
+    usr_last_msg = users_messages.get(message.from_user.id)
+    if not usr_last_msg:
+        users_messages[message.from_user.id] = datetime.now()
+        return False
+    else:
+        is_flood = (message.date - usr_last_msg).seconds < flood_timeout
+        users_messages[message.from_user.id] = datetime.now()
+        return is_flood
+
+
 def check_message_is_old(message: types.Message):
     return (datetime.now() - message.date).seconds > 300
 
@@ -43,7 +57,7 @@ async def process_callback_update_top(callback_query: types.CallbackQuery):
     chat_id = callback_query.message.chat.id
     message_id = callback_query.message.message_id
 
-    reply_text, reply_markup = await getTop()
+    reply_text, reply_markup = await get_top()
     reply_text += f'\n`Обновлено UTC {datetime.utcnow()}`'
     await bot.edit_message_text(text=reply_text, chat_id=chat_id, message_id=message_id, reply_markup=reply_markup, parse_mode=ParseMode.MARKDOWN)
 
@@ -64,20 +78,23 @@ async def on_msg(message: types.Message):
 
     # karma message
     if message.reply_to_message and message.reply_to_message.from_user.id and user_id != message.reply_to_message.from_user.id:
-        karma_changed = await increase_karma(message.reply_to_message.from_user.id, messageText)
-        if karma_changed:
-            msg = await bot.send_message(_chat_id, text=karma_changed, reply_to_message_id=message.message_id)
-            await autodelete_message(msg.chat.id, message_id=msg.message_id, seconds=destruction_timeout)
+        # check user on karmaspam
+        if not is_flood_message(message):
+            karma_changed = await increase_karma(message.reply_to_message.from_user.id, messageText)
+            if karma_changed:
+                msg = await bot.send_message(_chat_id, text=karma_changed, reply_to_message_id=message.message_id)
+                await autodelete_message(msg.chat.id, message_id=msg.message_id, seconds=destruction_timeout)
 
     # commands
     elif messageText == "карма":
         reply_text = await get_karma(user_id)
         msg = await bot.send_message(_chat_id, text=reply_text, parse_mode=ParseMode.MARKDOWN)
+        await autodelete_message(msg.chat.id, msg.message_id, destruction_timeout)
     elif messageText == "топ":
         global last_top
         top_list_destruction_timeout = 300
         if not last_top or (datetime.now(timezone.utc) - last_top).seconds > top_list_destruction_timeout:
-            reply_text, inline_kb = await getTop()
+            reply_text, inline_kb = await get_top()
             msg: types.Message = await bot.send_message(_chat_id, text=reply_text, reply_markup=inline_kb, parse_mode=ParseMode.MARKDOWN)
             last_top = datetime.now(timezone.utc)
             await autodelete_message(msg.chat.id, msg.message_id, top_list_destruction_timeout)
@@ -158,7 +175,7 @@ async def increase_karma(dest_user_id: int, message_text: str):
     return replytext
 
 
-async def getTop():
+async def get_top():
     replytext = "*Топ 10 кармы чата:*\n"
     users_list = [ v for k, v in users.items()]
     sorted_users_list = sorted(users_list, key = lambda i: i['karma'], reverse = True)[:10]
@@ -220,14 +237,6 @@ async def save_to_file(dict):
 async def autodelete_message(chat_id: int, message_id: int, seconds=0):
     await asyncio.sleep(seconds)
     await bot.delete_message(chat_id=chat_id, message_id=message_id)
-
-
-async def openFile():
-    if os.path.isfile(database_filename):
-        global users
-        users = eval(open(database_filename, 'r', encoding= 'utf-8').read())
-    else:
-        print ("File not exist")
 
 
 if __name__ == '__main__':
